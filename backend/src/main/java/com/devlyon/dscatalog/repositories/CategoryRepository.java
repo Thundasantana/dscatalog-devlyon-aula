bad4e8d22facf641eae03c836693ff6d8ddb60da
Ednilson Santana <ednilson.sdas@outlook.com>
2023-01-19 15:02:51 -0300

    H2 database

---
+++ backend/src/main/java/com/devlyon/dscatalog/repositories/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.devlyon.dscatalog.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.devlyon.dscatalog.entities.Category;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
---
+++ backend/src/main/java/com/devlyon/dscatalog/services/CategoryService.java
@@ -0,0 +1,20 @@
+package com.devlyon.dscatalog.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devlyon.dscatalog.entities.Category;
+import com.devlyon.dscatalog.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+}
