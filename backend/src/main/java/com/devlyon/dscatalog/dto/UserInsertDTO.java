2323ddaa0b0a1941c696edefcf3dd138bef41ddc
Thundasantana <ednilson.sdas@outlook.com>
2023-11-22 09:42:13 -0300

    Custom ConstraintValidator

--- backend/src/main/java/com/devlyon/dscatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/devlyon/dscatalog/dto/UserInsertDTO.java
@@ -1,5 +1,8 @@
 package com.devlyon.dscatalog.dto;
 
+import com.devlyon.dscatalog.services.validation.UserInsertValid;
+
+@UserInsertValid
 public class UserInsertDTO extends UserDTO {
 	private static final long serialVersionUID = 1L;
 	
